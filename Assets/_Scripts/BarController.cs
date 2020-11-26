b41f4b205fb2f96b3d893a0c4149509e17bfe155
Christopher Liu <30523986+CccrizzZ@users.noreply.github.com>
2020-11-27 07:05:29 +0800

    ##

---
+++ Assets/_Scripts/BarController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BarController : MonoBehaviour
+{
+    public Transform Bar;
+    public Transform TargetGameObject;
+    public int currentValue;
+    public int maxValue;
+
+    float counter;
+
+    void Start()
+    {
+        currentValue = 100;
+        maxValue = 100;
+    }
+
+    void Update()
+    {
+        if (TargetGameObject != null)
+        {
+            transform.position = TargetGameObject.position + Vector3.up;
+            
+        }
+    }
+
+    public void resetvalue()
+    {
+        // Bar.transform.position = transform.position+new Vector3(0.103f,0,0);
+        Bar.transform.position = new Vector3(
+            Bar.transform.position.x + counter,
+            transform.position.y,
+            transform.position.z
+        );
+        
+        
+        counter = 0;
+    }
+
+    public void SetValue(int newValue)
+    {
+        
+        if (newValue<currentValue)
+        {
+            Bar.transform.position = new Vector3(
+                Bar.transform.position.x - 0.104f, 
+                Bar.transform.position.y,
+                Bar.transform.position.z
+            );
+            counter += 0.104f;
+        }
+
+        currentValue = newValue;
+        Bar.localScale = new Vector3((float)((double) currentValue / (double) maxValue), 1.0f, 1.0f);
+
+        //clamp LocalScale min to 0
+        if (Bar.localScale.x < 0)
+        {
+            Bar.localScale = new Vector3(0.0f, 1.0f, 1.0f);
+        }
+    }
+}
---
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public Joystick joystick;
+    public float joystickHorizontalSensitivity;
+    public float joystickVerticalSensitivity;
+    public float horizontalForce;
+    public float verticalForce;
+    public bool isGrounded;
+    public bool isJumping;
+    public bool isCrouching;
+    public Transform spawnPoint;
+    public Transform lookAheadPoint;
+    public Transform lookInFrontPoint;
+    public LayerMask collisionGroundLayer;
+    public LayerMask collisionWallLayer;
+    public RampDirection rampDirection;
+    public bool onRamp;
+
+
+    
+    [Header("Health and Life")]
+    public int health;
+    public int lives;
+    public BarController HealthBar;
+    
+    
+    
+    private Rigidbody2D m_rigidBody2D;
+    private SpriteRenderer m_spriteRenderer;
+    private Animator m_animator;
+    private RaycastHit2D groundHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidBody2D = GetComponent<Rigidbody2D>();
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_animator = GetComponent<Animator>();
+        
+        
+        // Health and Life
+        health = 100;
+        lives = 5;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _LookInFront();
+        _LookAhead();
+        _Move();
+        Debug.Log(lives);
+    }
+
+    private void _LookInFront()
+    {
+        if (!isGrounded)
+        {
+            rampDirection = RampDirection.NONE;
+            return;
+        }
+
+        var wallHit = Physics2D.Linecast(transform.position, lookInFrontPoint.position, collisionWallLayer);
+        if (wallHit && isOnSlope())
+        {
+            rampDirection = RampDirection.UP;
+        }
+        else if (!wallHit && isOnSlope())
+        {
+            rampDirection = RampDirection.DOWN;
+        }
+
+        Debug.DrawLine(transform.position, lookInFrontPoint.position, Color.red);
+    }
+
+    private void _LookAhead()
+    {
+        groundHit = Physics2D.Linecast(transform.position, lookAheadPoint.position, collisionGroundLayer);
+
+        isGrounded = (groundHit) ? true : false;
+
+        Debug.DrawLine(transform.position, lookAheadPoint.position, Color.green);
+    }
+
+    private bool isOnSlope()
+    {
+        if (!isGrounded)
+        {
+            onRamp = false;
+            return false;
+        }
+
+        if (groundHit.normal != Vector2.up)
+        {
+            onRamp = true;
+            return true;
+        }
+
+        onRamp = false;
+        return false;
+    }
+
+    void _Move()
+    {
+        if (isGrounded)
+        {
+            if (!isJumping && !isCrouching)
+            {
+                if (joystick.Horizontal > joystickHorizontalSensitivity)
+                {
+                    // move right
+                    m_rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    if (onRamp && rampDirection == RampDirection.UP)
+                    {
+                        m_rigidBody2D.AddForce(Vector2.up * horizontalForce * 0.5f * Time.deltaTime);
+                    }
+                    else if (onRamp && rampDirection == RampDirection.DOWN)
+                    {
+                        m_rigidBody2D.AddForce(Vector2.down * horizontalForce * 0.5f * Time.deltaTime);
+                    }
+
+
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.RUN);
+                }
+                else if (joystick.Horizontal < -joystickHorizontalSensitivity)
+                {
+                    // move left
+                    m_rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                    if (onRamp && rampDirection == RampDirection.UP)
+                    {
+                        m_rigidBody2D.AddForce(Vector2.up * horizontalForce * 0.5f * Time.deltaTime);
+                    }
+                    else if (onRamp && rampDirection == RampDirection.DOWN)
+                    {
+                        m_rigidBody2D.AddForce(Vector2.down * horizontalForce * 0.5f * Time.deltaTime);
+                    }
+
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.RUN);
+                }
+                else
+                {
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.IDLE);
+                }
+            }
+
+            if ((joystick.Vertical > joystickVerticalSensitivity) && (!isJumping))
+            {
+                // jump
+                m_rigidBody2D.AddForce(Vector2.up * verticalForce);
+                m_animator.SetInteger("AnimState", (int) PlayerAnimationType.JUMP);
+                isJumping = true;
+            }
+            else
+            {
+                isJumping = false;
+            }
+
+            if ((joystick.Vertical < -joystickVerticalSensitivity) && (!isCrouching))
+            {
+                m_animator.SetInteger("AnimState", (int)PlayerAnimationType.CROUCH);
+                isCrouching = true;
+            }
+            else
+            {
+                isCrouching = false;
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // respawn
+        if (other.gameObject.CompareTag("DeathPlane"))
+        {
+            // transform.position = spawnPoint.position;
+            LoseLife();
+
+        }
+
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            TakeDamage(10);
+        }
+
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(10);
+        }
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(10);
+        }  
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        HealthBar.SetValue(health);
+
+        if (health <= 0)
+        {
+            
+            LoseLife();
+        }
+        
+    }
+
+    public void LoseLife()
+    {
+        lives -= 1;
+        if (lives > 0)
+        {
+            transform.position = spawnPoint.position;
+            health = 100;
+            HealthBar.SetValue(health);
+            HealthBar.resetvalue();
+        }
+        else
+        {
+            // goto gameover screen
+            Debug.Log("PLAYERDEAD");
+        }
+    }
+
+}
